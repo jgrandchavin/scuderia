c29277c1457c66279dbdada993cb058762c8afdb
Julien Grand-Chavin <hello@juliengrandchavin.dev>
2019-11-07 09:59:24 +0100

    Splash Screen

--- app/src/main/java/dev/juliengrandchavin/scuderia/SplashActivity.kt
+++ app/src/main/java/dev/juliengrandchavin/scuderia/SplashActivity.kt
@@ -3,7 +3,7 @@ package dev.juliengrandchavin.scuderia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
